bd11059420cff5e3af62b141e6f99359e1828552
Tsukasa Hamano <hamano@cuspy.org>
2008-09-30 02:43:49 +0900

    .emacs.d/init.el: added asm-mode settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,6 +140,7 @@
 
 (add-hook 'asm-mode-hook
           (lambda ()
+            (local-unset-key ";")
             (setq tab-width 4)
             (setq tab-stop-list
                   '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76))
