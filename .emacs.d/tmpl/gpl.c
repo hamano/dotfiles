47467433c823cc3886bf09218df2b732c0610227
Tsukasa Hamano <code@cuspy.org>
2008-11-10 14:14:34 +0900

    modified c template

--- .emacs.d/tmpl/gpl.c
+++ .emacs.d/tmpl/gpl.c
@@ -5,7 +5,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,13 +16,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-/* $Id: template.c,v 1.2 2007-06-05 13:25:21 hamano Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *args[]){
+int main(int argc, char *argv[]){
     if(argc < 2){
         fprintf(stderr, "too few arguments\n");
         return EXIT_FAILURE;
