a32f1397a0d66a0d0d98bffbb62494a2bdfd61ad
HAMANO Tsukasa <hamano@cuspy.org>
2013-04-18 13:16:02 +0900

    update firefox user.js

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,2 +1,12 @@
 
-user_pref("browser.cache.disk.parent_directory","/tmp/ffcache"); 
+user_pref("browser.cache.disk.parent_directory", "/tmp/ffcache");
+
+user_pref("browser.search.openintab", true);
+
+// disable mousewheel action
+// Firefox 16 or earlier
+//user_pref("mousewheel.withcontrolkey.action", 0);
+// Firefox 17 or later
+user_pref("mousewheel.with_control.action", 0);
+
+
