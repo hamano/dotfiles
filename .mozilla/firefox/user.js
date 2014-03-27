15b2de8facf01b7997f7c058806c743fd82b7a4c
HAMANO Tsukasa <hamano@cuspy.org>
2014-03-27 10:48:16 +0900

    update firefox config

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,12 +1,14 @@
 
-user_pref("browser.cache.disk.parent_directory", "/tmp/ffcache");
+var homepage = "http://www.google.co.jp/";
+user_pref("browser.startup.homepage", homepage);
+user_pref("browser.newtab.url", homepage);
 
 user_pref("browser.search.openintab", true);
+user_pref("browser.cache.disk.parent_directory", "/tmp/ffcache");
 
 // disable mousewheel action
 // Firefox 16 or earlier
 //user_pref("mousewheel.withcontrolkey.action", 0);
 // Firefox 17 or later
 user_pref("mousewheel.with_control.action", 0);
-
 
