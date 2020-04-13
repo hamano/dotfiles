0f8886b4a308198f5216a55502a18533e7f9f9ae
HAMANO Tsukasa <code@cuspy.org>
2020-04-14 00:10:43 +0900

    update awesome theme

--- .config/awesome/theme/gruvbox/theme.lua
+++ .config/awesome/theme/gruvbox/theme.lua
@@ -9,61 +9,74 @@ local bg1    = "#3c3836";
 local bg2    = "#504945";
 local bg3    = "#665c54";
 local bg4    = "#7c6f64";
-local gray   = "#928374";
-local fg     = "#ebdbb2";
 
-local bg     = "#282828"
-local red    = "#cc241d"
-local green  = "#98971a"
-local yellow = "#d79921"
-local blue   = "#458588"
-local purple = "#b16286"
-local aqua   = "#689d61"
+local bg = bg0;
+local fg = fg1;
 
-local b_blue = "#83a598"
+-- 16 colors
+local black   = "#282828";
+local red     = "#cc241d";
+local green   = "#98971a";
+local yellow  = "#d79921";
+local blue    = "#458588";
+local purple  = "#b16286";
+local aqua    = "#689d61";
+local white   = "#a89984";
+local b_black = "#928374";
+local b_red   = "#fb4934";
+local b_green = "#b8bb26";
+local b_yellow= "#fabd2f";
+local b_blue  = "#83a598";
+local b_purple= "#d3869b";
+local b_aqua  = "#8ec07c";
+local b_white = "#ebdbb2";
 
-local b_aqua = "#8ec07c"
+-- orange
+local orange  = "#d65d0e";
+local b_orange= "#fe8019";
 
 local dpi = require("beautiful.xresources").apply_dpi
 theme = dofile("/usr/share/awesome/themes/zenburn/theme.lua");
 theme.font      = "Ricty 11";
 
-local bg_normal = bg4
-local bg_focus = bg2
+local fg_focus  = fg
+local bg_focus = bg
+local fg_normal = fg3
+local bg_normal = bg1
 
---local bg_normal = b_aqua
---local bg_focus = aqua
-
--- {{{ Colors
-theme.fg_normal  = fg2
-theme.fg_focus   = bg0
-theme.fg_urgent  = fg0
-theme.bg_normal  = bg
-theme.bg_focus   = gray
-theme.bg_urgent  = red
-theme.bg_systray = bg
--- }}}
+theme.bg_focus   = bg_focus
+theme.bg_normal  = bg_normal
+theme.fg_focus   = fg_focus
+theme.fg_normal  = fg_normal
+theme.bg_urgent  = orange
+theme.bg_systray = bg_focus
 
 -- {{{ Borders
 theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(2)
-theme.border_normal = bg_normal
-theme.border_focus  = bg2
+theme.border_width  = dpi(1)
+theme.border_normal = fg_normal
+theme.border_focus  = fg_focus
 theme.border_marked = yellow;
 -- }}}
 
 -- {{{ Titlebars
-theme.titlebar_bg_focus  = gray
-theme.titlebar_bg_normal = bg
-theme.titlebar_fg_focus  = bg0
+theme.titlebar_bg_focus  = bg_focus
+theme.titlebar_bg_normal = bg_normal
+theme.titlebar_fg_focus  = fg_focus
+theme.titlebar_fg_normal  = fg_normal
 -- }}}
 
 local conf_dir = gears.filesystem.get_configuration_dir()
-local icon_dir = string.format("%s/theme/gruvbox/", conf_dir)
-theme.titlebar_close_button_normal = icon_dir .. "close_normal.png"
-theme.titlebar_close_button_focus  = icon_dir .. "close_focus.png"
+local theme_path = string.format("%s/theme/gruvbox/", conf_dir)
+theme.titlebar_close_button_normal = theme_path .. "close_normal.png"
+theme.titlebar_close_button_focus  = theme_path .. "close_focus.png"
 
-theme.titlebar_minimize_button_normal = icon_dir .. "minimize_normal.png"
-theme.titlebar_minimize_button_focus  = icon_dir .. "minimize_focus.png"
+theme.titlebar_minimize_button_normal = theme_path .. "minimize_normal.png"
+theme.titlebar_minimize_button_focus  = theme_path .. "minimize_focus.png"
+
+theme.titlebar_maximized_button_focus_active  = theme_path .. "maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = theme_path .. "maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme_path .. "maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme_path .. "maximized_normal_inactive.png"
 
 return theme;
