8406dc4c24d4f6b534ee0529afd1b710e7fc59aa
HAMANO Tsukasa <code@cuspy.org>
2020-04-23 16:00:42 +0900

    update awesome theme

--- .config/awesome/theme/gruvbox/theme.lua
+++ .config/awesome/theme/gruvbox/theme.lua
@@ -41,7 +41,7 @@ theme.font      = "Ricty 11";
 
 local fg_focus  = fg
 local bg_focus = bg
-local fg_normal = fg3
+local fg_normal = white
 local bg_normal = bg1
 
 theme.bg_focus   = bg_focus
@@ -78,5 +78,20 @@ theme.titlebar_maximized_button_focus_active  = theme_path .. "maximized_focus_a
 theme.titlebar_maximized_button_normal_active = theme_path .. "maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = theme_path .. "maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = theme_path .. "maximized_normal_inactive.png"
+
+theme.titlebar_ontop_button_focus_active  = theme_path .. "ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = theme_path .. "ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = theme_path .. "ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = theme_path .. "ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active  = theme_path .. "sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = theme_path .. "sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = theme_path .. "sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = theme_path .. "sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active  = theme_path .. "floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = theme_path .. "floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = theme_path .. "floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = theme_path .. "floating_normal_inactive.png"
 
 return theme;
