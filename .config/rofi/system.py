28005ed86d9a5e9ba27e34a788e6f1d48b0e8f11
HAMANO Tsukasa <code@cuspy.org>
2020-04-30 00:17:28 +0900

    update rofi settings

--- .config/rofi/system.py
+++ .config/rofi/system.py
@@ -4,17 +4,18 @@
 from collections import OrderedDict
 import os
 import sys
+import subprocess
 
 def main():
     menu = OrderedDict([
-#        ('Lock', 'dm-tool lock'),
-        ('Lock', 'dm-tool switch-to-greeter'),
+        ('Lock', 'dm-tool switch-to-greeter'), # dm-tool lock does not work...
+#        ('Lock', 'i3lock -c "#282828"'),
         ('Logout', 'dm-tool switch-to-greeter'),
         ('Reboot', 'systemctl reboot'),
         ('Poweroff', 'systemctl poweroff'),
     ])
     if len(sys.argv) > 1:
-        os.system(menu[sys.argv[1]])
+        subprocess.call(menu[sys.argv[1]], shell=True)
     else:
         for k in menu:
             print(k)
