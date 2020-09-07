a24677baf29da5364f67f305808cf660746e8ae0
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-09-08 01:14:53 +0900

    update rofi config

--- .config/rofi/system.py
+++ .config/rofi/system.py
@@ -8,15 +8,17 @@ import subprocess
 
 def main():
     menu = OrderedDict([
-        ('Lock', 'dm-tool lock'), # dm-tool lock does not work with lightdm-gtk-greeter
-#        ('Lock', 'dm-tool switch-to-greeter'), # if you use lightdm-gtk-greeter
+        ('Lock', 'dm-tool lock'),
 #        ('i3lock', 'i3lock -c "#282828"'),
         ('Logout', 'dm-tool switch-to-greeter'),
         ('Reboot', 'systemctl reboot'),
         ('Poweroff', 'systemctl poweroff'),
+        ('Screen Capure', "scrot -d 1 -e 'mv $f ~/Pictures/'"),
+        ('screen capure(focused)', "scrot -u -d 1 -e 'mv $f ~/Pictures/'"),
+        ('screen capure(select)', "scrot -s -e 'mv $f ~/Pictures/'"),
     ])
     if len(sys.argv) > 1:
-        subprocess.call(menu[sys.argv[1]], shell=True)
+        subprocess.Popen(menu[sys.argv[1]], shell=True, stdout=subprocess.PIPE)
     else:
         for k in menu:
             print(k)
