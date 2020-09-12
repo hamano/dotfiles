3b21c911907d9bc639787153a052fcfdee10c818
HAMANO Tsukasa <hamano@osstech.co.jp>
2020-09-13 00:18:41 +0900

    update script.py

--- .config/rofi/script.py
+++ .config/rofi/script.py
@@ -8,14 +8,13 @@ import subprocess
 
 def main():
     menu = OrderedDict([
-        ('Lock', 'dm-tool lock'),
-#        ('i3lock', 'i3lock -c "#282828"'),
-        ('Logout', 'dm-tool switch-to-greeter'),
-        ('Reboot', 'systemctl reboot'),
-        ('Poweroff', 'systemctl poweroff'),
-        ('Screen Capure', "scrot -d 1 -e 'mv $f ~/Pictures/'"),
-        ('screen capure(focused)', "scrot -u -d 1 -e 'mv $f ~/Pictures/'"),
-        ('screen capure(select)', "scrot -s -e 'mv $f ~/Pictures/'"),
+        ('System Lock', 'dm-tool lock'),
+        ('System Logout', 'dm-tool switch-to-greeter'),
+        ('System Reboot', 'systemctl reboot'),
+        ('System Poweroff', 'systemctl poweroff'),
+        ('Scrot focused', "scrot -u -d 1 -e 'mv $f ~/Pictures/'"),
+        ('Scrot select', "scrot -s -e 'mv $f ~/Pictures/'"),
+        ('Scrot full', "scrot -d 1 -e 'mv $f ~/Pictures/'"),
     ])
     if len(sys.argv) > 1:
         subprocess.Popen(menu[sys.argv[1]], shell=True, stdout=subprocess.PIPE)
