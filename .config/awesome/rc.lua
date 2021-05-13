f0a9d7e14cb3fb46077f1e98f36bdeb84d107606
HAMANO Tsukasa <hamano@osstech.co.jp>
2021-05-13 10:47:16 +0900

    update awesome config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -81,8 +81,8 @@ mod5 = "Mod5"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
