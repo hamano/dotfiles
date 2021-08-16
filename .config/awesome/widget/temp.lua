6a52a3d3db3533ce5e8cbca91c4e399f5513ed57
HAMANO Tsukasa <code@cuspy.org>
2021-08-17 01:45:04 +0900

    update temp widget

--- .config/awesome/widget/temp.lua
+++ .config/awesome/widget/temp.lua
@@ -5,19 +5,38 @@ local dpi = require("beautiful.xresources").apply_dpi
 local font = beautiful.font
 local lain = require("lain")
 local markup = lain.util.markup
+local pprint = require("util.pprint")
+local fs = require("gears.filesystem")
 
 local conf_dir = gears.filesystem.get_configuration_dir()
 local icon_dir = string.format("%s/icon/", conf_dir)
 local icon = wibox.widget.imagebox(icon_dir .. "temperature.svg")
+local fs = require("gears.filesystem")
+
+local tempfile
+-- for Intel
+if fs.file_readable("/sys/devices/virtual/thermal/thermal_zone0/temp") then
+  tempfile = "/sys/devices/virtual/thermal/thermal_zone0/temp"
+end
+-- for AMD
+if fs.file_readable("/sys/devices/platform/thinkpad_hwmon/hwmon/hwmon4/temp1_input") then
+  tempfile = "/sys/devices/platform/thinkpad_hwmon/hwmon/hwmon4/temp1_input"
+end
 
 local temp = lain.widget.temp {
+  tempfile = tempfile,
   settings = function()
-    now = math.floor(coretemp_now)
-    widget:set_markup(markup.font(font, string.format("%d%%", now)))
+--    now = math.floor(coretemp_now)
+    now = coretemp_now
+    if type(now) == "number" then
+      widget:set_markup(markup.font(font, string.format("%d℃", now)))
+    else
+      widget:set_markup(markup.font(font, now))
+    end
   end
 }
 
 local layout = wibox.layout.fixed.horizontal()
 layout:add(wibox.container.margin(icon, dpi(4), 0, dpi(4), dpi(4)))
-layout:add(wibox.container.margin(temp.widget, 0, 0, dpi(1.5), 0))
+layout:add(wibox.container.margin(temp.widget, 0, dpi(4), 0, 0))
 return layout
