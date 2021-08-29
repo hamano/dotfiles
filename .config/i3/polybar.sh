b0e51b7d973eb8b7343a65675dc190fb7ddf486e
HAMANO Tsukasa <code@cuspy.org>
2021-08-29 20:15:42 +0900

    update polybar.sh

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -5,17 +5,35 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
+
+function detect_temp() {
+    # for Intel
+    if [[ -f /sys/class/thermal/thermal_zone0/temp ]]; then
+        echo "/sys/class/thermal/thermal_zone0/temp"
+    fi
+    # for AMD
+    if [[ -f /sys/class/hwmon/hwmon4/temp1_input ]]; then
+        echo "/sys/class/hwmon/hwmon4/temp1_input"
+    fi
+    # for GPD
+    if [[ -f /sys/devices/virtual/thermal/thermal_zone6/temp ]]; then
+        echo "/sys/devices/virtual/thermal/thermal_zone6/temp"
+    fi
+}
+
 function start() {
+    export TEMP_HWMON_PATH=$(detect_temp)
     for d in $(polybar -m | cut -d: -f1); do
         MONITOR=$d polybar -r -c ~/.config/i3/polybar.conf top &
     done
 }
 
 function stop() {
-	pkill polybar
+    pkill polybar
 }
 
 case $1 in
     "start") start;;
     "stop") stop;;
 esac
+
