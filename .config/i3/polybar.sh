4f27874886ae92959c71e5afe5af4ef97c7619ac
HAMANO Tsukasa <code@cuspy.org>
2021-09-07 20:50:08 +0900

    update polybar.sh

--- .config/i3/polybar.sh
+++ .config/i3/polybar.sh
@@ -5,24 +5,32 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
-
-function detect_temp() {
+function set_env() {
     # for Intel
     if [[ -f /sys/class/thermal/thermal_zone0/temp ]]; then
-        echo "/sys/class/thermal/thermal_zone0/temp"
+        export TEMP_HWMON_PATH=/sys/class/thermal/thermal_zone0/temp
     fi
     # for AMD
     if [[ -f /sys/class/hwmon/hwmon4/temp1_input ]]; then
-        echo "/sys/class/hwmon/hwmon4/temp1_input"
+        export TEMP_HWMON_PATH=/sys/class/hwmon/hwmon4/temp1_input
     fi
     # for GPD
     if [[ -f /sys/devices/virtual/thermal/thermal_zone6/temp ]]; then
-        echo "/sys/devices/virtual/thermal/thermal_zone6/temp"
+        export TEMP_HWMON_PATH=/sys/devices/virtual/thermal/thermal_zone6/temp
+    fi
+    HOST=$(hostname)
+    if [[ $HOST == "x1c" ]]; then
+        export POLYBAR_ETH_INTERFACE=enp0s31f6
+        export POLYBAR_WLAN_INTERFACE=wlp2s0
+    fi
+    if [[ $HOST == "t14" ]]; then
+        export POLYBAR_ETH_INTERFACE=enp6s0
+        export POLYBAR_WLAN_INTERFACE=wlp3s0
     fi
 }
 
 function start() {
-    export TEMP_HWMON_PATH=$(detect_temp)
+    set_env
     for d in $(polybar -m | cut -d: -f1); do
         MONITOR=$d polybar -r -c ~/.config/i3/polybar.conf top &
     done
