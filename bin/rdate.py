bc3522959e8a25d2e8cd8b7707925ce148f11dab
HAMANO Tsukasa <code@cuspy.org>
2021-10-30 15:15:55 +0900

    update rdate

--- bin/rdate.py
+++ bin/rdate.py
@@ -3,6 +3,7 @@
 
 import sys
 import random
+import subprocess
 from datetime import timedelta
 from datetime import date
 from datetime import datetime
@@ -11,10 +12,13 @@ from datetime import timedelta
 from random import randrange
 
 def main():
+#   d = date.today()
     if len(sys.argv) > 1:
         d = datetime.strptime(sys.argv[1], '%Y-%m-%d')
     else:
-        d = date.today()
+        output = subprocess.check_output(["git log -1 --oneline --pretty=format:'%as'"], shell=True)
+        d = datetime.strptime(output.decode(), '%Y-%m-%d')
+        d = d + timedelta(days=1)
 
     delta = timedelta(seconds=randrange(24 * 60 * 60))
     today = datetime.combine(d, time(0))
