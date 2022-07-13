be27906ea3a404c465e18bc01b2351a6237f44ad
HAMANO Tsukasa <hamano@osstech.co.jp>
2022-07-13 15:32:52 +0900

    add resize-serial-console.sh

---
+++ bin/resize-serial-console.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# virsh consoleの画面サイズを調整する
+# /etc/profile.d に置くと便利
+# https://blog.n-z.jp/blog/2022-01-30-serial-console-resize.html
+# https://wiki.archlinux.org/title/working_with_the_serial_console#Resizing_a_terminal
+
+rsz () if [[ -t 0 ]]; then local escape r c prompt=$(printf '\e7\e[r\e[999;999H\e[6n\e8'); IFS='[;' read -sd R -p "$prompt" escape r c; stty cols $c rows $r; fi
+rsz
