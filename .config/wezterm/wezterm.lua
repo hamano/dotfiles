b0688a019bbca9a52d69f02d782d0e1b96ef69a6
HAMANO Tsukasa <hamano@osstech.co.jp>
2023-11-14 21:06:07 +0900

    set window_padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,13 +20,18 @@ config.keys = require 'keys'.keys
 config.enable_scroll_bar = true
 config.check_for_updates = false
 config.selection_word_boundary = '{}[]()"\'`,;: 　'
+config.window_padding = {
+  left = 16,
+  right = 16,
+  top = 0,
+  bottom = 0,
+}
 
 config.color_scheme = 'GruvboxDark'
 local scheme = wezterm.get_builtin_color_schemes()[config.color_scheme]
 config.colors = {
     scrollbar_thumb = scheme["foreground"]
 }
-
 -- config.pane_focus_follows_mouse = true
 -- config.debug_key_events = true
 return config
